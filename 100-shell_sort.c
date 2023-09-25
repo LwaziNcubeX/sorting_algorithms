c338823e9d8056e3c48af249effe6ef36861ad53
LwaziNcubeX <lwazincubex@gmail.com>
2023-09-26 00:13:27 +0200

    A Function that uses the Shell sort algorithm

---
+++ 100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sort an array of integers
+ * @array: The array to sort
+ * @size: The size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+	int tmp;
+
+	for (gap = 1; gap < size / 3; gap = gap * 3 + 1)
+	{
+
+	}
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = tmp;
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
+}
